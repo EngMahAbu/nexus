419030c39e0867eb796e8c9b145f068a7b8e75db
Mahmoud Ibrahim <mahmoud.ibrahim.saad.750@gmail.com>
2026-05-27 11:29:58 +0300

    Implemented API Hiding Mechanism
    - Implemented a mechanism to hide API keys for development builds. Unfortunately, it can't be used in production because it won't be reliable.

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:nexus/modules/auth/login_screen/login_screen.dart';
 import 'package:nexus/modules/home_layout/home_page.dart';
 import 'package:nexus/shared/components/constants.dart';
 import 'package:nexus/shared/network/local/shared_preferences_helper.dart';
 import 'package:nexus/shared/styles/themes.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  // Environment variables for securing API keys in dev builds [not production]
+  await dotenv.load(fileName: "lib/secrets.env");
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  );
   String? userId = await SharedPreferencesHelper.getString(userIdKey);
   runApp(MyApp(userId: userId));
 }
